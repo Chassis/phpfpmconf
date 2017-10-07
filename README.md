13745c76326b5b80a4bf722908a0b6be15acab2b
Bronson Quick <bronson@bronsonquick.com.au>
2017-10-07 12:15:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom PHP FPM Conf
+
+A Chassis extension that allows you to append new configuration values to your `php-fpm.conf` so you can tailor your Chassis box to match your server requirements.
---
+++ modules/phpfpmconf/manifests/init.pp
@@ -0,0 +1,37 @@
+# Setup our phpfpm configuration class
+class phpfpmconf (
+	$config,
+	$path = '/vagrant/extensions/phpfpmconf',
+) {
+	$version = $config[php]
+
+	if $version =~ /^(\d+)\.(\d+)$/ {
+		$package_version = "${version}.*"
+		$short_ver = $version
+	}
+	else {
+		$package_version = "${version}*"
+		$short_ver = regsubst($version, '^(\d+\.\d+)\.\d+$', '\1')
+	}
+
+	if versioncmp( $version, '5.4') <= 0 {
+		$php_package = 'php5'
+		$php_dir = 'php5'
+	}
+	else {
+		$php_package = "php${short_ver}"
+		$php_dir = "php/${short_ver}"
+	}
+
+	file { "/etc/${php_dir}/fpm/php-fpm.conf":
+		ensure  => 'present',
+		replace => false
+	}
+
+	file_line { 'append_conf':
+		line    => 'security.limit_extensions = php.html .php .html',
+		path    => "/etc/${php_dir}/fpm/php-fpm.conf",
+		require => Package["${php_package}-fpm"],
+		notify  => Service["${php_package}-fpm"],
+	}
+}
