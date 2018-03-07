7f56493be2b4836dacc4c73abc3b25b94ef3717b
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-07 14:17:44 +1000

    Add support for disabled extensions

--- modules/phpfpmconf/manifests/init.pp
+++ modules/phpfpmconf/manifests/init.pp
@@ -3,6 +3,12 @@ class phpfpmconf (
 	$config,
 	$path = '/vagrant/extensions/phpfpmconf',
 ) {
+	if ( ! empty( $config[disabled_extensions] ) and 'chassis/phpfpmconf' in $config[disabled_extensions] ) {
+		$file = absent
+	} else {
+		$file = present
+	}
+
 	# This is a dodgy hack to stop failures in Puppet if content/custom.conf doesn't exist.
 	if ( file('/vagrant/content/custom.conf', '/dev/null') != '' ) {
 		$version = $config[php]
@@ -31,8 +37,12 @@ class phpfpmconf (
 			require => Package["${php_package}-fpm"]
 		}
 
+		# Load in our custom conf so we can strip the carriage return and whitespace.
+		$conf = file('/vagrant/content/custom.conf')
+
 		file_line { 'append_conf':
-			line    => file('/vagrant/content/custom.conf'),
+			ensure  => $file,
+			line    => rstrip( $conf ),
 			path    => "/etc/${php_dir}/fpm/php-fpm.conf",
 			require => Package["${php_package}-fpm"],
 			notify  => Service["${php_package}-fpm"],
